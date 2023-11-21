cbfeb81f21401244e5776f94513842d75e99acc1
kwak <sal091625@gmail.com>
2023-11-21 20:04:30 +0900

    feat: 함수를 통해 코드를 간단히 수정

--- main.js
+++ main.js
@@ -2,72 +2,60 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, list, description){
+    return `
+        <!doctype html>
+        <html>
+        <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+        </head>
+        <body>
+        <h1><a href="/">WEB</a></h1>
+        ${list}
+        <h2>${title}</h2>
+        <p>${description}</p>
+        </body>
+        </html>
+    `;
+}
+
+function templateList(filelist){
+    var list = '<ul>';
+    for(i=0; i<filelist.length; i++){
+        list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    }
+    list = list + '</ul>';
+    return list;    
+}
+
 var app = http.createServer(function(request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathName = url.parse(_url, true).pathname;
-    var title = queryData.id;
 
     if(pathName === '/'){
-        if(title == undefined){ //root domain
-            fs.readdir('./data', function(err, filelist){
+        if(queryData.id == undefined){ //root domain
+            fs.readdir('./data', function(err, filelist){     
                 var title = 'Welcome';
-                var description = 'Hello Node.js';  
-                // var list = `<ol>
-                //     <li><a href="/?id=HTML">HTML</a></li>
-                //     <li><a href="/?id=CSS">CSS</a></li>
-                //     <li><a href="/?id=JavaScript">JavaScript</a></li>
-                // </ol>`;     
-                var list = '<ul>';
-                for(i=0; i<filelist.length; i++){
-                    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-                }
-                list = list + '</ul>'
-                var template = `
-                    <!doctype html>
-                    <html>
-                    <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                    </head>
-                    <body>
-                    <h1><a href="/">WEB</a></h1>
-                    ${list}
-                    <h2>${title}</h2>
-                    <p>${description}</p>
-                    </body>
-                    </html>`;
-                
+                var description ='Hello Node.js.';
+                var list = templateList(filelist);
+                var template = templateHTML(title, list, description);     
                 response.writeHead(200); //writeHead(200): 파일이 성공적으로 전송됨
                 response.end(template);                 
             })
         } else{
-            fs.readFile(`data/${title}`, 'utf8', function(err, description){
-                var template = `
-                <!doctype html>
-                <html>
-                <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-                </head>
-                <body>
-                <h1><a href="/">WEB</a></h1>
-                <ol>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                </ol>
-                <h2>${title}</h2>
-                <p>${description}</p>
-                </body>
-                </html>`;
+            fs.readdir('./data', function(err, filelist){
+                fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){   
+                    var title = queryData.id;
+                    var list = templateList(filelist);  
+                    var template = templateHTML(title, list, description);       
+                    response.writeHead(200); 
+                    response.end(template); 
+                });
+            })
             
-                response.writeHead(200); //writeHead(200): 파일이 성공적으로 전송됨
-                response.end(template); 
-            });
-        }
-        
-       
+        } 
     } else{
         response.writeHead(404); //writeHead(404): 에러
         response.end('Not Found');
